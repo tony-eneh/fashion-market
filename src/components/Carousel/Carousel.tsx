9c474e28dd98065cd1d53d8b16cbc94774741769
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2023-03-01 03:29:36 +0100

    feat: display carousel position indicators

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -4,20 +4,41 @@ export interface ICarouselProps {}
 
 export function Carousel(props: ICarouselProps) {
   return (
-    <div className='carousel-wrapper'>
+    <div className="carousel-wrapper">
       <div className="slide">
-        <div className="text-xs font-bold">#BEAUTYSALE</div>
+        <div className="flex justify-between">
+          <div className="text-xs font-bold">#BEAUTYSALE</div>
+          <div className="carousel-position-indicator">
+            <div className="dash"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+          </div>
+        </div>
         <p className="text-3xl font-semibold">DISCOVER OUR BEAUTY SELECTION</p>
         <button>Check this out</button>
       </div>
       <div className="slide">
-        <div className="text-xs font-bold">#FASHION DAY</div>
+        <div className="flex justify-between">
+          <div className="text-xs font-bold">#FASHION DAY</div>
+          <div className="carousel-position-indicator">
+            <div className="dot"></div>
+            <div className="dash"></div>
+            <div className="dot"></div>
+          </div>
+        </div>
         <div className="text-4xl font-bold">80% OFF</div>
         <p className="text-xs">Discover fashion that suits to your style</p>
         <button>Check this out</button>
       </div>
       <div className="slide">
-        <div className="text-xs font-bold">#KICKASS</div>
+        <div className="flex justify-between">
+          <div className="text-xs font-bold">#KICKASS</div>
+          <div className="carousel-position-indicator">
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dash"></div>
+          </div>
+        </div>
         <div className="text-4xl font-bold">70% OFF</div>
         <p className="text-xs">Discover classic shoes like crazy</p>
         <button>Check this out</button>
