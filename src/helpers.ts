436cbcddb8584fdd7356987718e39dd1ae571633
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2023-03-02 11:54:15 +0100

    fix: can shorten thousands

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,6 @@
 export function getColor(name?: string) {
     switch (name) {
-        case 'active': return '#80A499';
+        case 'active': return '#4ca78d';
         case 'muted': return '#B9BABC';
         case 'white': return 'white';
         case 'standard': return '#54545D';
@@ -9,5 +9,9 @@ export function getColor(name?: string) {
 }
 
 export function shortenNumber(num: number = 0) {
-    return '2.3k+'
+    if (num < 1000) {
+        return num
+    } else {
+        return `${(num / 1000).toFixed(1)}k+`
+    }
 }
